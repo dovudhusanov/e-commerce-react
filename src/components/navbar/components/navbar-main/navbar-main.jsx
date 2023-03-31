77e07b29509cd3e1f906ca43e932fa58918d8642
Dovudhon Husanov <95928909+husanovdovudkhon@users.noreply.github.com>
2023-03-31 18:37:49 +0500

    Update navbar-main.jsx

--- src/components/navbar/components/navbar-main/navbar-main.jsx
+++ src/components/navbar/components/navbar-main/navbar-main.jsx
@@ -27,9 +27,6 @@ function NavbarMain() {
 
     useEffect(() => {
         getUserInfo()
-        return () => {
-            setImgSrc(img)
-        }
     }, [window.location.pathname,])
 
     return (
