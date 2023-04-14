056aac07e41d68bc87107f8f5c0e98eb20bb5da1
Dovudhon Husanov <95928909+husanovdovudkhon@users.noreply.github.com>
2023-04-14 15:22:54 +0500

    Update index.jsx

--- src/pages/profile/pages/settings/components/user-settings-form/index.jsx
+++ src/pages/profile/pages/settings/components/user-settings-form/index.jsx
@@ -16,10 +16,10 @@ function UserSettingsForm() {
     async function getUserInfo() {
         setLoading(true)
         const userRes = await GetUserApi(localStorage.getItem("userId"))
-        localStorage.setItem("oldPhone", userRes.data[0].phone)
-        userRes?.data[0]?.profile && localStorage.setItem("profileId", userRes.data[0].profile)
-        const response = userRes?.data[0]?.profile && await UserInfoGetApi(localStorage.getItem("profileId"))
-        setProfileCreated(userRes[0]?.data)
+        localStorage.setItem("oldPhone", userRes.data.phone)
+        userRes?.data?.profile && localStorage.setItem("profileId", userRes.data.profile)
+        const response = userRes?.data.profile && await UserInfoGetApi(localStorage.getItem("profileId"))
+        setProfileCreated(userRes.data)
 
         setInputValue({
             first_name: response?.data[0].first_name,
