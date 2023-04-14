d00fa842f68c1758b996446bcfc852eb33319822
Dovudhon Husanov <95928909+husanovdovudkhon@users.noreply.github.com>
2023-04-14 16:28:35 +0500

    Update index.jsx

--- src/pages/profile/pages/settings/components/user-settings-form/index.jsx
+++ src/pages/profile/pages/settings/components/user-settings-form/index.jsx
@@ -16,16 +16,15 @@ function UserSettingsForm() {
     async function getUserInfo() {
         setLoading(true)
         const userRes = await GetUserApi(localStorage.getItem("userId"))
-        localStorage.setItem("oldPhone", userRes.data[0].phone)
-        userRes?.data[0]?.profile && localStorage.setItem("profileId", userRes.data[0].profile)
-        const response = userRes?.data[0].profile && await UserInfoGetApi(localStorage.getItem("profileId"))
-        setProfileCreated(userRes.data[0])
-        console.log(response)
+        localStorage.setItem("oldPhone", userRes.data.phone)
+        userRes?.data?.profile && localStorage.setItem("profileId", userRes.data.profile)
+        const response = userRes?.data?.profile && await UserInfoGetApi(localStorage.getItem("profileId"))
+        setProfileCreated(userRes?.data)
 
         setInputValue({
-            first_name: response?.data.first_name,
-            last_name: response?.data.last_name,
-            email: response?.data.email,
+            first_name: response?.data[0].first_name,
+            last_name: response?.data[0].last_name,
+            email: response?.data[0].email,
         });
         setLoading(false)
     }
@@ -52,8 +51,8 @@ function UserSettingsForm() {
         if (profileCreated.profile) {
             setIsLoading(true)
             const userRes = await GetUserApi(localStorage.getItem("userId"))
-            userRes?.data[0]?.profile && localStorage.setItem("profileId", userRes?.data[0].profile)
-            console.log(userRes.data[0].profile)
+            userRes?.data?.profile && localStorage.setItem("profileId", userRes?.data.profile)
+            console.log(userRes.data.profile)
             await UserInfoEditApi({
                 first_name, last_name, email
             }, localStorage.getItem("profileId"))
